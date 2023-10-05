9e5234ea2354cccad2a785bed9f0356d34e5536f
Adeyomola <kayzeel@yahoo.com>
2023-10-05 02:44:12 +0100

    new message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("On the wing"));
+app.get("/", (req, res) => res.send("Hot air balloon"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
